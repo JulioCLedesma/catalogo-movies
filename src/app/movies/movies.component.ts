010f5a986436110783072351c444937f3a62dd76
JULIO CESAR LEDESMA MARTINEZ <125784445+JulioCLedesma@users.noreply.github.com>
2024-10-13 11:43:15 -0600

    correo

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -10,6 +10,7 @@ import { AsyncPipe, JsonPipe, NgFor, NgIf } from '@angular/common';
   styleUrl: './movies.component.css'
 })
 export class MoviesComponent {
+  correo = 'julio.ledesma@udgvirtual.udg.mx';
   private readonly moviesSvc = inject(MovieService)
   movies$ = this.moviesSvc.getAllMovies();
 }
